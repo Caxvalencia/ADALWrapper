36d34cafa948813bfe67a8c2614573750e31b3b9
Daniel Rico <derico@bancolombia.com.co>
2018-11-26 16:44:57 -0500

    Base structure

---
+++ src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of bc-adal-angular
+ */
+
+export * from './lib/services/bc-adal-angular.service';
+export * from './lib/guards/adal-access.guard';
+export * from './lib/bc-adal-angular.module';
